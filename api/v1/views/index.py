7032a494cf271468cfbc4c783a6b6a49046337b8
Castro Hamiltone <castromiltone@gmail.com>
2023-09-30 23:45:39 +0300

    /stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,26 @@ module creates status route
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def show_status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def show_status():
+    amenities = storage.count('Amenity')
+    cities = storage.count('City')
+    places = storage.count('Place')
+    reviews = storage.count('Review')
+    states = storage.count('State')
+    users = storage.count('User')
+    return jsonify({
+        "amenities": amenities, 
+        "cities": cities, 
+        "places": places, 
+        "reviews": reviews, 
+        "states": states, 
+        "users": users
+    })
