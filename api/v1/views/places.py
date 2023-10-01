3e37a76091a61d9f9ad1d338d2670c7182bb1d02
Castro Hamiltone <castromiltone@gmail.com>
2023-10-01 20:18:01 +0300

    fix

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,12 +5,17 @@ module creates place route
 from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models import storage
+from models.city import City
+
 from models.place import Place
 
 
-@app_views.route('/places', methods=['GET', 'POST'], strict_slashes=False)
-def places():
+@app_views.route('/cities/<city_id>/places', methods=['GET', 'POST'], strict_slashes=False)
+def places(city_id=None):
     """places route"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404, 'Not found')
     if request.method == 'GET':
         all_places = storage.all('Place')
         places_list = []
@@ -24,6 +29,7 @@ def places():
             abort(400, 'Not a JSON')
         if json.get('name') is None:
             abort(400, 'Missing name')
+        json['city_id'] = city_id
         obj = Place(**json)
         obj.save()
         return jsonify(obj.to_dict()), 201
