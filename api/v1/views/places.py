3eb5bd63822ad195c554be15f7344120f8667c41
Castro Hamiltone <castromiltone@gmail.com>
2023-10-01 17:08:54 +0300

    api/v1/views/places.py

---
+++ api/v1/views/places.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+module creates place route
+"""
+from flask import jsonify, request, abort
+from api.v1.views import app_views
+from models import storage
+from models.place import Place
+
+
+@app_views.route('/places', methods=['GET', 'POST'], strict_slashes=False)
+def places():
+    """places route"""
+    if request.method == 'GET':
+        all_places = storage.all('Place')
+        places_list = []
+        for state in all_places.values():
+            places_list.append(state.to_dict())
+        return jsonify(places_list)
+
+    if request.method == 'POST':
+        json = request.get_json()
+        if json is None:
+            abort(400, 'Not a JSON')
+        if json.get('name') is None:
+            abort(400, 'Missing name')
+        obj = Place(**json)
+        obj.save()
+        return jsonify(obj.to_dict()), 201
+
+
+@app_views.route('/places/<place_id>', methods=['GET', 'DELETE', 'PUT'],
+                 strict_slashes=False)
+def place_id(place_id=None):
+    """state with id route"""
+    obj = storage.get(Place, place_id)
+    if obj is None:
+        abort(404, 'Not found')
+
+    if request.method == 'GET':
+        return jsonify(obj.to_dict())
+
+    if request.method == 'DELETE':
+        obj.delete()
+        del obj
+        return jsonify({}), 200
+
+    if request.method == 'PUT':
+        json = request.get_json()
+        if json is None:
+            abort(400, 'Not a JSON')
+        obj.update(json)
+        return jsonify(obj.to_dict()), 200
