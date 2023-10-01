4b7f39a2b337073379b91e31b996aa079a6876a4
Castro Hamiltone <castromiltone@gmail.com>
2023-10-01 16:06:19 +0300

    fix

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -12,26 +12,35 @@ from models.city import City
 @app_views.route('/cities', methods=['GET'], strict_slashes=False)
 def cities():
     """cities route"""
-    _cities = storage.all('City')
+    all_cities = storage.all(City)
     cities_list = []
-    for city in _cities.values():
+    for city in all_cities.values():
         cities_list.append(city.to_dict())
     return jsonify(cities_list)
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'],
+                 strict_slashes=False)
 def states_cities(state_id=None):
     """create cities route"""
-    json = request.get_json()
     state = storage.get(State, state_id)
     if state is None:
         abort(404, 'Not found')
-    if json is None:
-        abort(400, 'Not a JSON')
-    if json.get('name') is None:
-        abort(400, 'Missing name')
-    obj = City(**json)
-    obj.save()
-    return jsonify(obj.to_dict()), 201
+    if request.method == 'GET':
+        all_cities = storage.all(City)
+        cities = []
+        for city in all_cities:
+            if city.get('state_id') == state_id:
+                cities.append(city.to_dict())
+        return jsonify(cities), 200
+    if request.method == 'POST':
+        json = request.get_json()
+        if json is None:
+            abort(400, 'Not a JSON')
+        if json.get('name') is None:
+            abort(400, 'Missing name')
+        obj = City(**json)
+        obj.save()
+        return jsonify(obj.to_dict()), 201
 
 @app_views.route('/cities/<city_id>', methods=['GET', 'DELETE', 'PUT'],
                  strict_slashes=False)
